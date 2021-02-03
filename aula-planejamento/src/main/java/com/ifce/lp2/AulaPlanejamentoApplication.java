787bf5b5883a25312d1df7fae7f01ce297d691cf
Thiago Felippe <thiago@lar.ifce.edu.br>
2021-02-03 15:56:01 -0300

    #Planejamento: Projeto inicializado

---
+++ aula-planejamento/src/main/java/com/ifce/lp2/AulaPlanejamentoApplication.java
@@ -0,0 +1,13 @@
+package com.ifce.lp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaPlanejamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaPlanejamentoApplication.class, args);
+	}
+
+}
