e28776aced649ad6c4d0f5a6ee61abae4abf5a36
Thiago Felippe <thiago@lar.ifce.edu.br>
2021-02-17 16:11:57 -0300

    Init status code

---
+++ status-codes/src/main/java/br/edu/ifce/StatusCodesApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusCodesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatusCodesApplication.class, args);
+	}
+
+}
