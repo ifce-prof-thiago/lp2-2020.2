2632efba484fb5c2099caedefe19c29a2a9cf860
Thiago Felippe <thiago@lar.ifce.edu.br>
2021-03-18 15:51:51 -0300

    Git init - Ecommerce-book

---
+++ e-commerce-book/src/test/java/lp2/ifce/edu/br/ECommerceBookApplicationTests.java
@@ -0,0 +1,13 @@
+package lp2.ifce.edu.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
